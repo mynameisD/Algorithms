77c76a1c839b86726b177df4764c2b34020e23f7
Di Xu <sirxudi@gmail.com>
2016-03-11 19:37:09 -0500

    hw2 beta1

---
+++ week2/Subset.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import java.util.*; 
+
+public class Subset {
+    public static void main(String[] args) {
+        
+        int N = Integer.parseInt(args[0]);  
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()){
+            q.enqueue(StdIn.readString());  
+        }
+        Iterator<String> qI=q.iterator();  
+        for (int i = 0; i < N; i++) {
+            StdOut.println(qI.next());
+        }
+    }
+}
