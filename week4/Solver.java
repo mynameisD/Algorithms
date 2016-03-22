d9909cb4a307ebcc4487bcc94c0990f44b6029f5
mynameisD <sirxudi@gmail.com>
2016-03-22 10:11:57 -0400

    week4

--- week4/Solver.java
+++ week4/Solver.java
@@ -6,7 +6,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
-    private int move = 0; //total number of moves 
+    private int move = -1; //total number of moves 
     private boolean solvable = true;  
     private Queue<Board> minpath = null;  //result slover
     
@@ -15,11 +15,13 @@ public class Solver {
         private int steps;  
         private Board prev;  // the previous 
         private int rank;  //previouse moves + manhattan distance
+        private Queue<Board> path;  
         
-        public SearchNode(Board b, int m, Board pre) {
+        public SearchNode(Board b, int m, Board pre, Queue<Board> p) {
             this.board = b;  
             this.steps = m; 
-            this.prev = pre;
+            this.prev = pre;  
+            this.path = p;  
             this.rank = m + b.manhattan();  
         }
     }
@@ -37,30 +39,36 @@ public class Solver {
         
         MinPQ<SearchNode> q1 = new MinPQ<SearchNode>(new NodeComparator());
         MinPQ<SearchNode> q2 = new MinPQ<SearchNode>(new NodeComparator()); 
-        q1.insert(new SearchNode(initial, 0, null));  
-        q2.insert(new SearchNode(initial.twin(), 0, null));  
-        
-        Queue<Board> path1 = new Queue<Board>();
+        Queue<Board> path = new Queue<Board>();
+        path.enqueue(initial);  
+        q1.insert(new SearchNode(initial, 0, null, path));  
+        q2.insert(new SearchNode(initial.twin(), 0, null, null));  
         
         SearchNode cur1,cur2; 
         
         do {
             cur1 = q1.delMin();  
-            cur2 = q2.delMin();  
-            path1.enqueue(cur1.board);    
+            cur2 = q2.delMin();     
+            
+            //copy previous path
             
             for(Board next:cur1.board.neighbors()) {
-                if (!next.equals(cur1.prev)) q1.insert(new SearchNode(next,cur1.steps+1,cur1.board));
+                if (!next.equals(cur1.prev)) {
+                    path = new Queue<Board>();  
+                    for(Board pre:cur1.path) {path.enqueue(pre); } 
+                    path.enqueue(next);  
+                    q1.insert(new SearchNode(next,cur1.steps+1,cur1.board, path));
+                }
             }
             for(Board next:cur2.board.neighbors()) {
-                if (!next.equals(cur2.prev)) q2.insert(new SearchNode(next,cur2.steps+1,cur2.board)); 
+                if (!next.equals(cur2.prev)) q2.insert(new SearchNode(next,cur2.steps+1,cur2.board, null)); 
             }
             
         } while (!cur1.board.isGoal() && !cur2.board.isGoal());
         
         if(cur1.board.isGoal()) {
             this.move = cur1.steps;   
-            this.minpath = path1;  
+            this.minpath = cur1.path;  
         }
         else {this.solvable = false;  }       
     }
